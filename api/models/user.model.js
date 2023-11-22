e967e9a1b79fddd48eb794d25fbd51071643f1e5
John Waweru <johnwaweru092@gmail.com>
2023-11-23 02:03:29 +0300

    added user model

---
+++ api/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+// schema is used for rules and conditions
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// create model
+const User = mongoose.model("User", userSchema);
+
+export default User;
